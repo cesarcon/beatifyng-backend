c1644f7a962640efdb0026c7abfe1f4bf9a8827f
Jeimy <jeimyt91@hotmail.com>
2023-11-04 17:58:56 -0500

    "incoportacion de libreria Lombok a entidad DetalleFactura  para reduccion del tamaño del codigo en construcor con argumentos, constructor vacio, metodos Get y Set"

--- src/main/java/com/beatifying/backend/entities/DetalleFactura.java
+++ src/main/java/com/beatifying/backend/entities/DetalleFactura.java
@@ -1,7 +1,15 @@
 package com.beatifying.backend.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "detalles_factura")
 public class DetalleFactura {
@@ -17,44 +25,4 @@ public class DetalleFactura {
     @JoinColumn(name = "id_servicio")
     private Servicio servicio;
 
-
-    public DetalleFactura(int idDetallesFactura, int cantidad, double totalDetalle, double iva, int idFactura, int idServicios) {
-        this.idDetallesFactura = idDetallesFactura;
-        this.cantidad = cantidad;
-        this.totalDetalle = totalDetalle;
-        this.iva = iva;
-    }
-
-    public int getIdDetallesFactura() {
-        return idDetallesFactura;
-    }
-
-    public void setIdDetallesFactura(int idDetallesFactura) {
-        this.idDetallesFactura = idDetallesFactura;
-    }
-
-    public int getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
-    }
-
-    public double getTotalDetalle() {
-        return totalDetalle;
-    }
-
-    public void setTotalDetalle(double totalDetalle) {
-        this.totalDetalle = totalDetalle;
-    }
-
-    public double getIva() {
-        return iva;
-    }
-
-    public void setIva(double iva) {
-        this.iva = iva;
-    }
-
-}
+   }
