f650de4710fe664901fc504c41de171aee927937
Jeimy <jeimyt91@hotmail.com>
2023-11-04 17:55:40 -0500

    incoportacion de libreria Lombok a entidad categoria para reduccion del tamaño del codigo en construcor con argumentos, constructor vacio, metodos Get y Set

--- src/main/java/com/beatifying/backend/entities/Categoria.java
+++ src/main/java/com/beatifying/backend/entities/Categoria.java
@@ -1,6 +1,12 @@
 package com.beatifying.backend.entities;
 import jakarta.persistence.*;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "categorias")
 
@@ -11,43 +17,4 @@ public class Categoria {
     private String nombre;
     private String descripcion;
     private Integer cantidad;
-
-    public Categoria(Integer idCategoria, String nombre, String descripcion, Integer cantidad) {
-        this.idCategoria = idCategoria;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.cantidad = cantidad;
     }
-
-    public Integer getIdCategoria() {
-        return idCategoria;
-    }
-
-    public void setIdCategoria(Integer idCategoria) {
-        this.idCategoria = idCategoria;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public Integer getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(Integer cantidad) {
-        this.cantidad = cantidad;
-    }
-}
