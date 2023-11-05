4051a48edbc72b4ae3bac664878ba545cce278fa
Jeimy <jeimyt91@hotmail.com>
2023-11-04 21:42:10 -0500

    "correcion @RequestMapping("/factura") se elimina la varible name por que no aplica"

--- src/main/java/com/beatifying/backend/controllers/CategoriaController.java
+++ src/main/java/com/beatifying/backend/controllers/CategoriaController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+@RestController
+@RequestMapping("/categorias")
 
 public class CategoriaController {
     @Autowired
